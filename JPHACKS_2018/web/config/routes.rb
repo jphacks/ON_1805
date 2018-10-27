9c4594c1cbc524f7cef9b9a9983761a0e4457870
RyumaRyama <homes.ryu@gmail.com>
2018-10-27 10:37:44 +0900

    userページの表示

--- JPHACKS_2018/web/config/routes.rb
+++ JPHACKS_2018/web/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'top/home'
   get 'aggregate/show'
   get 'users/new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
