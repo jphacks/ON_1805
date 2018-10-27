52b3c7d6c58049a15f2a4c2bfea3f2c6fc541bdb
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-27 11:27:00 +0900

    createアクションの生成

--- JPHACKS_2018/web/app/controllers/users_controller.rb
+++ JPHACKS_2018/web/app/controllers/users_controller.rb
@@ -5,9 +5,24 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:ie_number, :email,
+                                   :gpa, :lab,
+                                   :password,
+                                   :password_confirmation)
+    end
 end
