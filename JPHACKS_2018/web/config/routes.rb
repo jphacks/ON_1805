1b6ecde949312b08b1a0a1b98a3a2b1532cd69e9
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-27 19:51:49 +0900

    login,logout機構の実装

--- JPHACKS_2018/web/config/routes.rb
+++ JPHACKS_2018/web/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get '/', to: "users#index"
+  get 'sessions/new'
+  root "users#index"
   get 'aggregate/show'
   resources :users
   post '/signup',  to: 'users#create'
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
