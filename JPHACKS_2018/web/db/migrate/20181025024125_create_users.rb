e794259ca43e6e5e732b52ed15fad62dfd2a5fe1
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-28 10:28:54 +0900

    herokuへのDeploy

--- JPHACKS_2018/web/db/migrate/20181025024125_create_users.rb
+++ JPHACKS_2018/web/db/migrate/20181025024125_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :ie_number
-      t.references :laboratory, foreign_key: true
+      # t.references :laboratory, foreign_key: true
       t.string :email
-      t.string :gpa
+      t.float :gpa
 
       t.timestamps
     end
--- JPHACKS_2018/web/db/schema.rb
+++ JPHACKS_2018/web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_053834) do
+ActiveRecord::Schema.define(version: 2018_10_27_150544) do
 
   create_table "laboratories", force: :cascade do |t|
     t.string "name"
@@ -20,12 +20,12 @@ ActiveRecord::Schema.define(version: 2018_10_27_053834) do
 
   create_table "users", force: :cascade do |t|
     t.string "ie_number"
-    t.integer "laboratory_id"
     t.string "email"
-    t.string "gpa"
+    t.float "gpa"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "laboratory_id"
     t.index ["laboratory_id"], name: "index_users_on_laboratory_id"
   end
 
