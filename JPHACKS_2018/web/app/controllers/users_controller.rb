fd90b8c5143b9a566f90a890f430909df1f3fd30
RyumaRyama <homes.ryu@gmail.com>
2018-10-28 11:27:13 +0900

    user情報の編集が可能

--- JPHACKS_2018/web/app/controllers/users_controller.rb
+++ JPHACKS_2018/web/app/controllers/users_controller.rb
@@ -21,9 +21,26 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      flash[:success] = "Welcome to GPA war"
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新成功
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
